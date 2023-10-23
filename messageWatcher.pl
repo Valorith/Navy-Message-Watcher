43b34478bed68c469aaff17681ca4df19a859a27
Valorith <rgagnier06@gmail.com>
2023-10-22 23:10:39 -0400

    Add data saving and loading

--- messageWatcher.pl
+++ messageWatcher.pl
@@ -3,6 +3,9 @@ use warnings;
 use LWP::Simple;
 use HTML::TableExtract;
 
+#load data from csv file
+load_from_csv();
+
 #store the current year in a variable
 my $year = (localtime)[5] + 1900;
 
@@ -28,6 +31,18 @@ our @messages;
 our @subjects;
 our @dates;
 our @urls;
+
+# Initialize new arrays
+our @newMessages;
+our @newSubjects;
+our @newDates;
+our @newUrls;
+
+#clear the arrays
+@messages = ();
+@subjects = ();
+@dates = ();
+@urls = ();
 
 # Extract all hrefs that include "/Portals/55/Messages/NAVADMIN/NAV$year" in the URL
 @urls = $html_content =~ /href="(.*?\/Portals\/55\/Messages\/NAVADMIN\/NAV$year.*?)"/g;
@@ -59,6 +74,29 @@ print "Found " . scalar @messages . " messages\n";
 #print a debug statement showing the number of urls found
 print "Found " . scalar @urls . " urls\n";
 
+
+my $messageCount = @messages;
+my $newMessageCount = @newMessages;
+
+#If new messages > old messages: There are new messages
+
+if ($newMessageCount > $messageCount) {
+    print "\e[32mThere are new messages!\e[0m\n";
+    print "New messages: " . ($newMessageCount - $messageCount) . "\n";
+    print "Old messages: $messageCount\n";
+
+    #copy the new arrays to the old arrays
+    @messages = @newMessages;
+    @subjects = @newSubjects;
+    @dates = @newDates;
+    @urls = @newUrls;
+
+} else {
+    print "\e[31mThere are no new messages.\e[0m\n";
+    print "New messages: $newMessageCount\n";
+    print "Old messages: $messageCount\n";
+}
+
 my $index = 0;
 foreach my $message (@messages) {
 
@@ -79,4 +117,59 @@ foreach my $message (@messages) {
 
     print $index + 1 . ") Message: $message || " . "Subject: " . $subjects[$index] . " || Date: " . $dates[$index] . " || " . $urls[$index] . "\n";
     $index++;
+}
+
+write_to_csv();
+
+sub write_to_csv {
+    use Text::CSV;
+
+    my $csv = Text::CSV->new({ binary => 1, auto_diag => 1, eol => "\n" });
+    my $filename = "data.csv";
+
+    open(my $fh, ">:encoding(utf8)", $filename) or die "Could not open '$filename' for writing: $!";
+
+    # Write headers to CSV file
+    $csv->print($fh, ["Message", "Subject", "Date", "URL"]);
+
+    # Write data to CSV file
+    for (my $i = 0; $i < scalar @messages; $i++) {
+        $csv->print($fh, [$messages[$i], $subjects[$i], $dates[$i], $urls[$i]]);
+    }
+
+    close $fh;
+}
+
+sub load_from_csv {
+    use Text::CSV;
+
+    my $csv = Text::CSV->new({ binary => 1, auto_diag => 1, eol => "\n" });
+    my $filename = "data.csv";
+
+    unless (-e $filename) {
+        warn "\e[31mFile '$filename' does not exist.\e[0m";
+        return;
+    }
+
+    open(my $fh, "<:encoding(utf8)", $filename) or warn "\e[31mCould not open '$filename' for reading: $!\e[0m";
+
+    # Read headers from CSV file
+    my $headers = $csv->getline($fh);
+
+    #clear the arrays
+    @newMessages = ();
+    @newSubjects = ();
+    @newDates = ();
+    @newUrls = ();
+
+    # Read data from CSV file
+    while (my $row = $csv->getline($fh)) {
+        push @newMessages, $row->[0];
+        push @newSubjects, $row->[1];
+        push @newDates, $row->[2];
+        push @newUrls, $row->[3];
+    }
+
+    close $fh;
+
 }
